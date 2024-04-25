704f1ec1dabf4cd9dcebea8e2445d7bb41b36628
Thierry <thierrytorquato@gmail.com>
2024-04-24 22:04:12 -0300

    atualizado views.py e urls.py

--- src/django/analisadores/urls.py
+++ src/django/analisadores/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.Analise.as_view()),
+    path('api/resolve/', views.resolve, name='resolve'),
 ]
--- src/django/analisadores/views.py
+++ src/django/analisadores/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
 from .classes import lexico
 
 # Create your views here.
-class Analise(View):
-    def get(self, request):
-        analisador_lexico = lexico.Lexic()
+def resolve(request):
+    if request.method == 'POST':
+        string = request.POST.get()
 
-        try:
-            analisador_lexico.check(request)
-        except Exception as e:
-            return HttpResponse(e)
-    
-        return HttpResponse('Sucesso')
+        return HttpResponse(string)
